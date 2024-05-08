1cce485e57da719a811ee8c71a996a48e3d7879c
s23009 <s23009@std.it-college.ac.jp>
2024-05-08 16:07:25 +0900

    6章終わり

---
+++ src/main/kotlin/Chap06.kt
@@ -0,0 +1,17 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val num = 100
+    val a: Boolean = (num == 100)
+    val b: Boolean = (num ==125)
+    println("変数 a の値は $a です。")
+    println("変数 b の値は $b です。")
+    println("~~~~6.6~~~~")
+    val numb61 = 5
+    val numb62 = 10
+    val a6 = (numb61 < numb62)
+    val b6 = (numb61 > numb62)
+    println("num61 < num62より小さい:$a6")
+    println("num61 < num62より大きい:$b6")
+
+}
