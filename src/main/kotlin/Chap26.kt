790e789ddeb30b57aa8d6ac9cbb9ccc6092a59e0
itc-s23009 <s23009@std.it-college.ac.jp>
2024-05-21 11:52:23 +0900

    26章終了

--- src/main/kotlin/Chap26.kt
+++ src/main/kotlin/Chap26.kt
@@ -5,43 +5,102 @@ import java.time.LocalDateTime
 
 fun main(args: Array<String>) {
     // プライマリコンストラクタのみを使う場合
-    val book = Book(
-        "やさしい Kotlin 入門", "野崎 英一",
-        LocalDate.of(2018, 5, 10),
-        RentalInfo(
-            "タケシ", LocalDateTime.now(),
-            LocalDateTime.of(2024, 5, 31, 12, 0, 0)
-        )
+    val book1 = Book(
+        100,
+        "やさしい Kotlin 入門",
+        "野崎 英一",
+        LocalDate.of(2018, 5, 10).atStartOfDay(),
+        LocalDateTime.of(2024, 5, 31, 12, 0, 0)
     )
+
+    val rentalInfo1 = RentalInfo(
+        "タケシ", LocalDateTime.now(),
+        LocalDateTime.of(2024, 5, 31, 12, 0, 0)
+    )
+
+    val rental1 = Rental(book1, true)  // Rental クラスのインスタンスを作成
+    val bookWithRental1 = BookWithRental(
+        book1, rental1
+    )
+    val bookInfo1 = BookInfo(
+        bookWithRental1.book.id,
+        bookWithRental1.book.title,
+        bookWithRental1.book.author,
+        bookWithRental1.rental.isRental
+    )
+    bookInfo1.printInfo()
+
+    // 別のBookインスタンスを作成
+    val book2 = Book(
+        101,
+        "たけし",
+        "著者",
+        LocalDateTime.now(),
+        LocalDateTime.of(2024, 5, 28, 0, 0, 0)
+    )
+    val rental2 = Rental(book2, true)  // Rental クラスのインスタンスを作成
+    val bookWithRental2 = BookWithRental(
+        book2, rental2
+    )
+    val bookInfo2 = BookInfo(
+        bookWithRental2.book.id,
+        bookWithRental2.book.title,
+        bookWithRental2.book.author,
+        bookWithRental2.rental.isRental
+    )
+    bookInfo2.printInfo()
+
+    val bookInfo3 = BookInfo(
+        book2.id,
+        book2.title,
+        book2.author,
+        true
+    )
+    bookInfo3.printInfo()
 }
 
-class Book(
+data class Book(
+    val id: Long,
     val title: String,
     val author: String,
-    val releaseDate: LocalDate,
-    val rentalInfo: RentalInfo
+    val publishDate: LocalDateTime,
+    val returnDate: LocalDateTime
+)
+
+data class Rental(
+    val book: Book,
+    val isRental: Boolean
+)
+
+data class BookWithRental(
+    val book: Book,
+    val rental: Rental
+)
+
+class BookInfo(
+    val id: Long,
+    val title: String,
+    val author: String,
+    val isRental: Boolean
 ) {
+    constructor(bookWithRental: BookWithRental) : this(
+        bookWithRental.book.id,
+        bookWithRental.book.title,
+        bookWithRental.book.author,
+        bookWithRental.rental.isRental
+    )
+
     fun printInfo() {
         println(
             """
-            ------------------------------
-            書籍情報
-            ------------------------------
-            $title
-            $releaseDate 初版
-            著者 $author
-            ------------------------------
-            貸出情報
-            ------------------------------
-            借りてるユーザー: ${rentalInfo.userName}
-            借りた日: ${rentalInfo.rentalDate}        
-            返却期限: ${rentalInfo.returnDeadline}
-        """.trimIndent()
+                [ID${id}] $title (${author})
+                ${if (isRental) "貸出中です" else "貸出可能です"}
+            """.trimIndent()
         )
     }
 }
 
-class RentalInfo(
+data class RentalInfo(
     val userName: String,
     val rentalDate: LocalDateTime,
     val returnDeadline: LocalDateTime
