d73295136487739788b516a8bde280878f7b4d2f
itc-s23009 <s23009@std.it-college.ac.jp>
2024-05-23 12:18:15 +0900

    36章終了

---
+++ src/main/kotlin/Chap36.kt
@@ -0,0 +1,53 @@
+package com.example.chap36
+
+fun main(args: Array<String>) {
+    run{
+        println("----36.1----")
+        val p = Person1()
+        p.great()
+    }
+    run{
+        println("----36.2----")
+        val p = Person2()
+        p.great()
+        p.great2()
+    }
+    run {
+        println("----36.3----")
+        val p = Person3()
+        p.setNameAndAge("ヒロイン", 20)
+        p.great()
+    }
+
+}
+class Person1 {
+    var name = "名無しさん"
+    var age = 0
+    fun great() {
+        println("${name}です。${age}歳です。")
+    }
+}
+class Person2 {
+    var name = "名無しさん"
+    var age = 0
+    fun great(){
+        val name = "ひろし"
+        val age = 46
+        println("${name}です。${age}歳です。")
+    }
+    fun great2(){
+        println("${name}です。${age}歳です")
+    }
+}
+class Person3 {
+    var name = ""
+    var age = 0
+
+    fun setNameAndAge(name: String, age: Int) {
+        this.name = name
+        this.age = age
+    }
+    fun great() {
+        println("${name}です。${age}歳です")
+    }
+}
